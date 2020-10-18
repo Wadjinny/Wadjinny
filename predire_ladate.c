4143a9623f5dec66ceaa28c95c5cc69f0f991476
ilyas wadjinny <ilyasswadjinny@gmail.com>
2020-10-18 13:57:15 +0200

    first commit

---
+++ predire_ladate.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{   
+    int jours,mois,annee;
+    printf("taper le jours");
+    scanf("%d %d %d",jours,mois,annee);
+    if(jours==31){
+        if(mois==1 & mois==3 & mois==5 & mois==7 & mois==8 & mois==10){
+            
+        }else{
+            if(mois==12){
+
+            }else{
+
+            }
+        }
+    }
+    printf("taper le mois");
+    printf("taper le l'annee:");
+    return 0;
+}
